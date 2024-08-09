e50b8dbb845e54934ed278ab586e2861cc923fc4
Marvinkwame <marvinkudjo@gmail.com>
2024-08-09 01:47:33 +0000

    Drizzle orm setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,15 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 //import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
+import * as schema from "./schema"
 
 // for migrations
 //const migrationClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db", { max: 1 });
 //migrate(drizzle(migrationClient), ...)
 
 // for query purposes
-const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db");
-const db = drizzle(queryClient);
+const queryClient = postgres(process.env.DATABASE_URL!);
+const db = drizzle(queryClient, { schema });
 
 
 //await db.select().from(...)...
